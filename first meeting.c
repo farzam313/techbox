124cd0485f68f7a52fdb098556d2977d05288f00
farzam313 <107337733+farzam313@users.noreply.github.com>
2022-06-12 10:21:43 +0530

    Create first meeting.c

---
+++ first meeting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char name[35]="";
+    printf("please Enter your name");
+    scanf("%s",&name);
+    printf("Nice to meet you Mr/Ms %s and i am Farzam ",name);
+    printf("SEE YOU SOON, GOD BYE");
+    getch();
+}
